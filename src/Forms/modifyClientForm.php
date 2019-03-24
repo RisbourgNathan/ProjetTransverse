6ed675ef5528e07feb817656f3372fa714aca520
raphagoo <raphael.ago@hotmail.fr>
2019-03-24 20:04:01 +0100

    Modify infos on a client form

---
+++ src/Forms/modifyClientForm.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: RAGOSTINI
+ * Date: 21/12/2018
+ * Time: 15:47
+ */
+
+namespace App\Forms;
+use App\Entity\Agency;
+use App\Entity\User;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+
+class modifyClientForm extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('email', EmailType::class)
+            ->add('firstname', TextType::class)
+            ->add('lastname', TextType::class)
+            ->add('city', TextType::class)
+            ->add('zipcode', NumberType::class)
+            ->add('street', TextType::class)
+            ->add('phone', NumberType::class)
+            ->add('plainPassword', RepeatedType::class, array(
+                'type' => PasswordType::class,
+                'first_options'  => array('label' => 'Password'),
+                'second_options' => array('label' => 'Repeat Password'),
+            ))
+            ->add('agency',EntityType::class, array('class' => Agency::class, 'choice_label' => 'name', 'mapped' => false))
+        ;
+    }
+}
