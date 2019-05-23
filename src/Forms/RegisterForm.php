321b575dbd5338c095fb15efdf66b88f47b07dd5
raphagoo <raphael.ago@hotmail.fr>
2019-05-24 00:54:38 +0200

    Fix and improvements on numerous files

--- src/Forms/RegisterForm.php
+++ src/Forms/RegisterForm.php
@@ -23,22 +23,52 @@ class RegisterForm extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('email', EmailType::class)
-            ->add('firstname', TextType::class)
-            ->add('lastname', TextType::class)
-            ->add('city', TextType::class)
-            ->add('zipcode', NumberType::class)
-            ->add('street', TextType::class)
-            ->add('phone', NumberType::class)
+            ->add('email', EmailType::class, [
+                'label' => 'Email',
+                'label_attr' => ['class' => 'sr-only', 'for' => 'inputEmail'],
+                'attr' => ['class' => 'form-control', 'placeholder' => 'Email', 'id' => 'inputEmail'],
+            ])
+            ->add('firstname', TextType::class, [
+                'label' => 'Prénom',
+                'label_attr' => ['class' => 'sr-only', 'for' => 'inputFirstName'],
+                'attr' => ['class' => 'form-control', 'placeholder' => 'Prénom', 'id' => 'inputFirstName'],
+            ])
+            ->add('lastname', TextType::class, [
+                'label' => 'Nom',
+                'label_attr' => ['class' => 'sr-only', 'for' => 'inputName'],
+                'attr' => ['class' => 'form-control', 'placeholder' => 'Nom', 'id' => 'inputNae'],
+            ])
+            ->add('city', TextType::class, [
+                'label' => 'Ville',
+                'label_attr' => ['class' => 'sr-only', 'for' => 'inputCity'],
+                'attr' => ['class' => 'form-control', 'placeholder' => 'Ville', 'id' => 'inputCity'],
+            ])
+            ->add('zipcode', NumberType::class, [
+                'label' => 'Code postal',
+                'label_attr' => ['class' => 'sr-only', 'for' => 'inputPostalCode'],
+                'attr' => ['class' => 'form-control', 'placeholder' => 'Code Postal', 'id' => 'inputPostalCode'],
+            ])
+            ->add('street', TextType::class, [
+                'label' => 'Rue',
+                'label_attr' => ['class' => 'sr-only', 'for' => 'inputStreet'],
+                'attr' => ['class' => 'form-control', 'placeholder' => 'Rue', 'id' => 'inputStreet'],
+            ])
+            ->add('phone', NumberType::class,[
+                'label' => 'Numéro de téléphone',
+                'label_attr' => ['class' => 'sr-only', 'for' => 'inputPhone'],
+                'attr' => ['class' => 'form-control', 'placeholder' => 'Numéro de téléphone', 'id' => 'inputPhone'],
+            ])
             ->add('plainPassword', RepeatedType::class, array(
                 'type' => PasswordType::class,
-                'first_options'  => array('label' => 'Password'),
-                'second_options' => array('label' => 'Repeat Password'),
+                'first_options'  => array('label' => 'Mot de passe', 'label_attr' => ['class' => 'sr-only', 'for' => 'inputFirstPassword'], 'attr' => ['class' => 'form-control', 'placeholder' => 'Mot de passe', 'id' => 'inputFirstPassword']),
+                'second_options' => array('label' => 'Répéter le mot de passe', 'label_attr' => ['class' => 'sr-only', 'for' => 'inputSecondPassword'], 'attr' => ['class' => 'form-control', 'placeholder' => 'Répéter le mot de passe', 'id' => 'inputSecondPassword']),
             ))
             ->add('sponsorCode', TextType::class, [
-                'label' => 'Sponsor\'s code',
+                'label' => 'Code de parrainage',
                 'required' => false,
-                'mapped' => false
+                'mapped' => false,
+                'label_attr' => ['class' => 'sr-only', 'for' => 'inputSponsorCode'],
+                'attr' => ['class' => 'form-control', 'placeholder' => 'Code de parrainage', 'id' => 'inputSponsorCode'],
             ])
         ;
     }
